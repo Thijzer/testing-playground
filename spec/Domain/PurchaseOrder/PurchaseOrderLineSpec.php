eb9911d02f71ba29a8a5cb4c232c83e04fee96eb
skeleton <marie.bochu@akeneo.com>
2018-04-16 14:20:01 +0200

    Add validation on Product Order Line

--- spec/Domain/PurchaseOrder/PurchaseOrderLineSpec.php
+++ spec/Domain/PurchaseOrder/PurchaseOrderLineSpec.php
@@ -8,13 +8,37 @@ use Prophecy\Argument;
 
 class PurchaseOrderLineSpec extends ObjectBehavior
 {
-    function let()
-    {
-        $this->beConstructedWith('nexus-6', 100);
-    }
-
     function it_is_initializable()
     {
+        $this->beConstructedWith('nexus-6', 100);
+
         $this->shouldHaveType(PurchaseOrderLine::class);
+    }
+
+    function it_throws_an_exception_if_quantity_is_negative()
+    {
+        $this->beConstructedWith('nexus-6', -1);
+
+        $this->shouldThrow(
+            \InvalidArgumentException::class
+        )->duringInstantiation();
+    }
+
+    function it_throws_an_exception_if_quantity_is_equal_to_zero()
+    {
+        $this->beConstructedWith('nexus-6', 0);
+
+        $this->shouldThrow(
+            \InvalidArgumentException::class
+        )->duringInstantiation();
+    }
+
+    function it_throws_an_exception_if_product_id_is_empty()
+    {
+        $this->beConstructedWith('', 100);
+
+        $this->shouldThrow(
+            \InvalidArgumentException::class
+        )->duringInstantiation();
     }
 }
--- src/Domain/PurchaseOrder/PurchaseOrderLine.php
+++ src/Domain/PurchaseOrder/PurchaseOrderLine.php
@@ -12,6 +12,18 @@ class PurchaseOrderLine
 
     public function __construct(string $productId, float $quantity)
     {
+        if ($quantity < 0) {
+            throw new \InvalidArgumentException('Quantity cannot be negative');
+        }
+
+        if ((float) 0 === $quantity) {
+            throw new \InvalidArgumentException('Quantity cannot be equal to 0');
+        }
+
+        if ('' === $productId) {
+            throw new \InvalidArgumentException('Product ID cannot be empty');
+        }
+
         $this->productId = $productId;
         $this->quantity = $quantity;
     }
