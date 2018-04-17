71cc1641b0055d1064aec3ce5357d1d0fbd161b3
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2018-04-17 09:17:12 +0200

    Placing an order

--- spec/Domain/PurchaseOrder/PurchaseOrderSpec.php
+++ spec/Domain/PurchaseOrder/PurchaseOrderSpec.php
@@ -10,21 +10,46 @@ use Prophecy\Argument;
 
 class PurchaseOrderSpec extends ObjectBehavior
 {
-    function it_is_initializable(PurchaseOrderId $id, PurchaseOrderLine $purchaseOrderLine)
+    function let(PurchaseOrderId $id, PurchaseOrderLine $purchaseOrderLine)
     {
         $this->beConstructedWith($id, [$purchaseOrderLine], 'yumyum');
+    }
+
+    function it_is_initializable()
+    {
         $this->shouldHaveType(PurchaseOrder::class);
+        $this->placed()->shouldReturn(false);
     }
 
     function it_can_be_constructed_with_no_line(PurchaseOrderId $id)
     {
         $this->beConstructedWith($id, [], 'yumyum');
         $this->shouldHaveType(PurchaseOrder::class);
+        $this->placed()->shouldReturn(false);
     }
 
-    function it_adds_a_line_to_an_order(PurchaseOrderId $id, PurchaseOrderLine $purchaseOrderLine)
+    function it_adds_a_line_to_an_order(PurchaseOrderLine $purchaseOrderLine)
     {
-        $this->beConstructedWith($id, [$purchaseOrderLine], 'yumyum');
         $this->addLine($purchaseOrderLine);
+    }
+
+    function it_places_an_order()
+    {
+        $this->place();
+        $this->placed()->shouldReturn(true);
+    }
+
+    function it_can_place_an_order_already_placed_is_ok()
+    {
+        $this->place();
+        $this->place();
+        $this->placed()->shouldReturn(true);
+    }
+
+    function it_does_not_place_an_order_without_lines(PurchaseOrderId $id)
+    {
+        $this->beConstructedWith($id, [], 'yumyum');
+        $this->place();
+        $this->placed()->shouldReturn(false);
     }
 }
--- src/Domain/PurchaseOrder/PurchaseOrder.php
+++ src/Domain/PurchaseOrder/PurchaseOrder.php
@@ -13,15 +13,31 @@ class PurchaseOrder
     /** @var string */
     private $supplierId;
 
+    /** @var bool */
+    private $placed;
+
     public function __construct(PurchaseOrderId $id, array $purchaseOrderLines, string $supplierId)
     {
         $this->purchaseOrderLines = $purchaseOrderLines;
         $this->supplierId = $supplierId;
         $this->id = $id;
+        $this->placed = false;
     }
 
     public function addLine(PurchaseOrderLine $purchaseOrderLine): void
     {
         $this->purchaseOrderLines[] = $purchaseOrderLine;
+    }
+
+    public function place(): void
+    {
+        if (!empty($this->purchaseOrderLines)) {
+            $this->placed = true;
+        }
+    }
+
+    public function placed(): bool
+    {
+        return $this->placed;
     }
 }
